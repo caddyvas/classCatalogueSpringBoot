656a8201ca5d631dece6cfe495470e05a91f2df8
Deepak Rajendran <deepakrajendran@Deepaks-MacBook-Pro.local>
2022-04-20 13:12:12 +0530

    firstCommit

---
+++ src/test/java/com/deep/classcatalogue/classcatalogue/ClassCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deep.classcatalogue.classcatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
